a2547b2464714178bac8ef8f1aa62c69be6c305c
Tapias <tapia2390@gmail.com>
2025-02-12 09:59:07 -0500

    aprendiendo sprint boot

---
+++ src/main/java/com/sprintBoot/sprint_boot/SprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.sprintBoot.sprint_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprintBoot/sprint_boot/SprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintBoot.sprint_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
